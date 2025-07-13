6e335846b1d75f5a19f8be9ed1ded2be33f00f72
AbhishekGupta <=>
2025-07-13 13:54:05 +0530

    all

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const app = express();
+const cookieParser = require("cookie-parser");
+
+app.use(cookieParser());
 
 //cookie concepts
 app.get("/getCookies",(req,res)=>{
     res.cookie("mycookie","hello");
      res.cookie("manu","namaste");
     res.send("Cookie is implement");
-})
+});
+
+app.get("/",(req,res)=>{
+    console.dir(req.cookies);
+    res.send("send your some cookies!");
+
+});
 
 
 
